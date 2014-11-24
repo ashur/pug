e62ecf8fab0a86926af863d44d8fa7acea6e84e4
Ashur Cabrera <hello@cabreramade.co>
2014-11-23 19:55:38 -0800

    moved project functionality to 'track'

--- app/commands.php
+++ app/commands.php
@@ -6,63 +6,26 @@
 
 $commands = [];
 
-// Project
-$project = new Huxtable\Command('project', 'List, create or delete projects', function()
+$commands[] = new Huxtable\Command('list', 'List all tracked projects', function()
 {
 	$pug = new Pug\Pug();
 	listProjects($pug->getProjects());
 });
 
-$projectAdd = new Huxtable\Command('add', 'Add a project named <name> which lives at <path>.', function($name, $path)
+$commands[] = new Huxtable\Command('track', 'Track a project named <name> which lives at <path>', function($name, $path='')
 {
 	$pug = new Pug\Pug();
 	$pug->addProject(new Pug\Project($name, $path));
 	listProjects($pug->getProjects());
 });
 
-$projectDisable = new Huxtable\Command('disable', 'Disable an existing project named <name>.', function($name)
-{
-	$pug = new Pug\Pug();
-	$pug->disableProject($name);
-	listProjects($pug->getProjects());
-});
-
-$projectEnable = new Huxtable\Command('enable', 'Enable a disabled project named <name>.', function($name)
-{
-	$pug = new Pug\Pug();
-	$pug->enableProject($name);
-	listProjects($pug->getProjects());
-});
-
-$projectRemove = new Huxtable\Command('remove', 'Remove the project named <name>.', function($name)
+$commands[] = new Huxtable\Command('untrack', 'Stop tracking the project named <name>.', function($name)
 {
 	$pug = new Pug\Pug();
 	$pug->removeProject($name);
 	listProjects($pug->getProjects());
 });
 
-$projectSetPath = new Huxtable\Command('set-path', 'Changes the path for the named project.', function($name, $path)
-{
-	$pug = new Pug\Pug();
-	$pug->setPathForProject(new Pug\Project($name, $path));
-});
-
-$projectShow = new Huxtable\Command('show', 'Show details for the project named <name>.', function($name)
-{
-	$pug = new Pug\Pug();
-	listProjects(array($pug->getProject($name)), true);
-});
-
-$project->addSubcommand($projectAdd);
-$project->addSubcommand($projectDisable);
-$project->addSubcommand($projectEnable);
-$project->addSubcommand($projectRemove);
-$project->addSubcommand($projectSetPath);
-$project->addSubcommand($projectShow);
-
-$commands[] = $project;
-
-// Update
 $update = new Huxtable\Command('update', 'Fetch project updates', function($app)
 {
 	$pug = new Pug\Pug();
@@ -77,14 +40,11 @@ $commands[] = $update;
  * @param	array	$projects
  * @param	boolean	$showPath
  */
-function listProjects(array $projects, $showPath=false)
+function listProjects(array $projects)
 {
 	foreach($projects as $project)
 	{
-		$enabled = $project->isEnabled() === true ? '*' : '-';
-		$pattern = $showPath ? " %s %-16s%-20s%s\n" : " %s %-16s%s\n";
-
-		printf($pattern, $enabled, $project->getName(), $project->getUpdated(), $project->getPath());
+		printf(" %-16s%-20s%s\n", $project->getName(), $project->getUpdated(), $project->getPath());
 	}
 }
 
