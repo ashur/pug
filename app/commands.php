d98687d97569689c10dda533f279acc6cf7e2d7c
Ashur Cabrera <hello@cabreramade.co>
2014-11-23 21:03:48 -0800

    improved path resolution and added name defaulting

--- app/commands.php
+++ app/commands.php
@@ -6,26 +6,54 @@
 
 $commands = [];
 
+// --
+// list
+// --
 $commands[] = new Huxtable\Command('list', 'List all tracked projects', function()
 {
 	$pug = new Pug\Pug();
 	listProjects($pug->getProjects());
 });
 
-$commands[] = new Huxtable\Command('track', 'Track a project named <name> which lives at <path>', function($name, $path='')
+// --
+// track
+// --
+$commands[] = new Huxtable\Command('track', 'Track a project at <path>', function($path, $name='')
 {
+	$file = new SplFileInfo($path);
+
+	// Resolve $path
+	if(!file_exists($file->getRealPath()))
+	{
+		throw new \Huxtable\Command\CommandInvokedException("Couldn't track project, path '{$path}' not found", 1);
+	}
+	if(!$file->isDir())
+	{
+		throw new \Huxtable\Command\CommandInvokedException("Couldn't track project, path '{$path}' not a directory", 1);
+	}
+
+	$name = strlen($name) > 0 ? $name : $file->getFilename();
+
 	$pug = new Pug\Pug();
-	$pug->addProject(new Pug\Project($name, $path));
+	$pug->addProject(new Pug\Project($name, $file->getRealPath()));
+
 	listProjects($pug->getProjects());
 });
 
-$commands[] = new Huxtable\Command('untrack', 'Stop tracking the project named <name>.', function($name)
+// --
+// untrack
+// --
+$commands[] = new Huxtable\Command('untrack', 'Stop tracking the project <name>.', function($name)
 {
 	$pug = new Pug\Pug();
 	$pug->removeProject($name);
+
 	listProjects($pug->getProjects());
 });
 
+// --
+// update
+// --
 $update = new Huxtable\Command('update', 'Fetch project updates', function($app)
 {
 	$pug = new Pug\Pug();
