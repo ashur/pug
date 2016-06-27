566ced9d35ffa69ed40317bb9e915faebfa5d518
Ashur Cabrera <hello@cabreramade.co>
2016-06-26 19:09:52 -0700

    don't ignore the app config file

---
+++ lib/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	/*
+	 * Application name
+	 */
+	'name' => 'pug',
+
+	/*
+	 * Application version
+	 */
+	'version' => '0.5.0b1',
+
+	/*
+	 * PHP minimum version
+	 */
+	'php-min' => '5.4'
+);
